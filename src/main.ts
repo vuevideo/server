6f69f2ed55d60a20609e17513f4cac42e5942775
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-04-15 15:55:04 +0530

    Setup versioning for APIs. Closes #2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
