e16771198439d14bbe8a0496bdcee4f441d90c52
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-05-02 20:12:41 +0530

    Prisma Setup for NestJS Server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   app.setGlobalPrefix('api');
   app.enableVersioning({
