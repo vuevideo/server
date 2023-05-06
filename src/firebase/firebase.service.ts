84c7ce9a7f08a2f7607cc1a76aaf1a7655306813
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-05-06 20:54:10 +0530

    Firebase Authentication Module Added

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,13 +1,21 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { App, initializeApp, applicationDefault } from 'firebase-admin/app';
+import { Auth, getAuth } from 'firebase-admin/auth';
 
 @Injectable()
 export class FirebaseService implements OnModuleInit {
-  private firebase: App;
+  private _firebase: App;
+  private _auth: Auth;
 
   onModuleInit() {
-    this.firebase = initializeApp({
+    this._firebase = initializeApp({
       credential: applicationDefault(),
     });
+
+    this._auth = getAuth();
+  }
+
+  public get auth() {
+    return this._auth;
   }
 }
