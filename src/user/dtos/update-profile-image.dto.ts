ee126b11fcabfdd52b8db73a9c40f8d239009874
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-07-16 17:27:04 +0530

    Rename Update Image DTO

--- src/user/dtos/update-profile-image.dto.ts
+++ src/user/dtos/update-profile-image.dto.ts
@@ -1,16 +1,16 @@
 import { IsUUID, IsUrl } from 'class-validator';
 
-export class UpdateProfileImage {
+export class UpdateProfileImageDto {
   @IsUrl(
     {},
     {
       message: 'Image URL is not a valid URL.',
     },
   )
-  public imageUrl: string;
+  public imageLink: string;
 
   @IsUUID('4', {
     message: 'Image UUID is not a valid UUID.',
   })
-  public imageUuid: string;
+  public storageUuid: string;
 }
