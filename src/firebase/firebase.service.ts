75684842d4570daaf49902856cfc872443ae5563
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-07-16 15:10:21 +0530

    Firebase Storage Added.

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -1,11 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { App, initializeApp, applicationDefault } from 'firebase-admin/app';
 import { Auth, getAuth } from 'firebase-admin/auth';
+import { Storage, getStorage } from 'firebase-admin/storage';
 
 @Injectable()
 export class FirebaseService implements OnModuleInit {
   private _firebase: App;
   private _auth: Auth;
+  private _storage: Storage;
 
   onModuleInit() {
     this._firebase = initializeApp({
@@ -13,9 +15,14 @@ export class FirebaseService implements OnModuleInit {
     });
 
     this._auth = getAuth();
+    this._storage = getStorage();
   }
 
   public get auth() {
     return this._auth;
+  }
+
+  public get storage() {
+    return this._storage;
   }
 }
