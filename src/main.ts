f0039bbf91d4d9efadf1bd69b40c4dd8c0f1aed3
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2023-07-23 15:39:36 +0530

    Logging Implemented. Closes #53

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI,
   });
+
+  app.use(morgan('combined'));
 
   await app.listen(process.env.PORT || 3000);
 }
