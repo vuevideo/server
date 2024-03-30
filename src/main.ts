d91098e10146753f7f93597de2194121c5e2e150
Varun Khatri <varun.h.khatri@gmail.com>
2024-03-30 16:03:19 +0530

    Update Prisma Service for NestJS

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
 
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
